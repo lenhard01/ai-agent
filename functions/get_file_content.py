3a307f6ca589008b0b7b4292a297884078ce7f83
Lenhard Hailar <149786490+lenhard01@users.noreply.github.com>
2026-09-20 01:48:59 +0800

    feat(functions): implement get_file_content with truncation

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,6 +1,27 @@
-def get_file_content(working_directory: str, file_path: str) -> str:
-    if file_path != working_directory:
-        return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+import os
 
-    if not os.path.isfile(file_path):
-        return f'Error: File not found or is not a regular file: "{file_path}"'
+from config import MAX_CHARS
+
+
+def get_file_content(working_directory: str, file_path: str) -> str:
+    try:
+        working_dir_abs = os.path.abspath(working_directory)
+        target_abs = os.path.normpath(os.path.join(working_dir_abs, file_path))
+
+        if os.path.commonpath([working_dir_abs, target_abs]) != working_dir_abs:
+            return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+
+        if not os.path.isfile(target_abs):
+            return f'Error: File not found or is not a regular file: "{file_path}"'
+
+        with open(target_abs) as f:
+            file_content_string = f.read(MAX_CHARS)
+
+            if f.read(1):
+                file_content_string += (
+                    f'[...File "{file_path}" truncated at {MAX_CHARS} characters]'
+                )
+
+        return file_content_string
+    except Exception as e:
+        return f"Error: {e}"
